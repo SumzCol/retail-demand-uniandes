de79ed706a5bb7b569a8422f6f35bd80216bc793
kevinmal123 <kevin.malamboa@gmail.com>
2025-09-17 22:06:11 -0500

    pipeline_sarimax_1

---
+++ src/retail_demand_uniandes/pipelines/modelo_sarimax/prubas.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+df_resultados = pd.read_parquet("data/04_feature/resultados_modelos_sarimax.parquet")
+df_resumen = pd.read_parquet("data/05_model_input/resumen_top_modelos.parquet")
+
+print(df_resultados.head())
+print(df_resumen.head())
